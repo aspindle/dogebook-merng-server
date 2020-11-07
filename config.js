5fbac6c6384f89b59335e432f239f568d0235442
Alex Spindler <>
2020-11-06 17:32:10 -0800

    hope this works

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://dogebookuser:GLtcArsGsOKkcJsA@cluster0.l7nnp.mongodb.net/dogebook-merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'a very secrety secret key'
+}
